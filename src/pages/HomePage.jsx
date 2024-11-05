3a04c011d5d17a24b1df6fd961ec6584c88b39f7
Max de Vocht <maxdevocht@hotmail.com>
2024-11-05 20:57:44 +0100

    Update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { headerData } from "@/assets/assets";
-import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 
 const HomePage = () => {
   return (
@@ -35,17 +35,26 @@ const HomePage = () => {
         {headerData.map((item, index) => (
           <div key={index} className="relative rounded-2xl overflow-hidden">
             <div className="flex h-72">
-              <div className="self-end ml-auto">
-                <Button
-                  variant="secondary"
-                  className="text-xs font-sans tracking-widest"
-                >
-                  {item.title}
-                </Button>
+              <div className="self-end ml-auto z-50">
+                <div className="flex gap-2 bg-black px-4 py-2 mb-2 mr-2 rounded-2xl shadow-lg">
+                  <button className="text-sm tracking-widest">
+                    {item.title}
+                  </button>
+                  <div className="border border-border-default rounded-full bg-background-muted p-2">
+                    <ArrowRight />
+                  </div>
+                </div>
               </div>
               <div className="absolute inset-0 -z-10">
                 <img src={item.image.src} alt={item.image.alt} className="" />
               </div>
+              <div
+                className="absolute bottom-0 left-0 right-0 h-24"
+                style={{
+                  background:
+                    "linear-gradient(to top, rgba(0, 0, 0, 0.5), transparent)",
+                }}
+              />
             </div>
           </div>
         ))}
