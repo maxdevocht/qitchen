cca8c315c4ee2fe5afe296791c66d961c8e990fa
Max de Vocht <maxdevocht@hotmail.com>
2024-11-05 12:45:53 +0100

    Created Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import { NavLink } from "react-router-dom";
 
 const topLineVariants = {
   open: {
@@ -45,32 +46,43 @@ const bottomLineVariants = {
 };
 
 const Navbar = () => {
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav>
-      <div>
-        <img src="./logo.svg" alt="" />
-        <button
-          className="-mr-2 flex size-12 flex-col items-center justify-center md:hidden"
-          onClick={() => setIsMobileMenuOpen((prev) => !prev)}
-        >
-          <motion.span
-            className="my-[3px] h-0.5 w-6 bg-black"
-            animate={isMobileMenuOpen ? ["open", "rotatePhase"] : "closed"}
-            variants={topLineVariants}
-          />
-          <motion.span
-            className="my-[3px] h-0.5 w-6 bg-black"
-            animate={isMobileMenuOpen ? "open" : "closed"}
-            variants={middleLineVariants}
-          />
-          <motion.span
-            className="my-[3px] h-0.5 w-6 bg-black"
-            animate={isMobileMenuOpen ? ["open", "rotatePhase"] : "closed"}
-            variants={bottomLineVariants}
-          />
-        </button>
+    <nav className="absolute top-12 left-12 border border-border-default rounded-lg p-2">
+      <div className="flex items-center justify-between">
+        <NavLink to={"/"} className="flex items-center">
+          <img src="./Logo.svg" alt="Qitchen logo" />
+        </NavLink>
+
+        <div className="flex mr-4 ml-16">
+          <ul className="flex items-center gap-4">
+            <NavLink className="font-sans font-light tracking-wider">
+              MENU
+            </NavLink>
+            <NavLink className="font-sans font-light tracking-wider">
+              ABOUT
+            </NavLink>
+          </ul>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <button className="font-sans font-light tracking-wider py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100">
+            BOOK A TABLE
+          </button>
+
+          <button
+            className={`hamburger hamburger--minus flex items-center py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100 ${
+              isMenuOpen ? "is-active" : ""
+            }`}
+            type="button"
+            onClick={() => setIsMenuOpen((prev) => !prev)}
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
       </div>
     </nav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  darkMode: ["class"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-  	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-  		colors: {}
-  	}
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {
+        text: {
+          default: "#EFE7D2",
+          muted: "#F5F2EA",
+          inverse: "#0A0B0A",
+        },
+        background: {
+          default: "#0A0B0A",
+          muted: "#181818",
+          "muted-hover": "#1E1E1E",
+          primary: "#EFE7D2",
+          "primary-hover": "#CFBE91",
+        },
+        border: {
+          default: "#333330",
+          "default-hover": "#4E4C47",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Satoshi", "sans-serif"], // Satoshi as the default sans-serif
+      forum: ["Forum", "serif"], // Forum as a custom font
+    },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
