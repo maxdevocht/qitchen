50dd88de7703b5b9a71ab051fd66684aa39811b1
Max de Vocht <maxdevocht@hotmail.com>
2024-11-05 20:50:45 +0100

    Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,18 +7,17 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="flex items-center m-auto w-full">
-      <div className="flex items-center gap-3">
-        <NavLink>
-          <img src="./Logo.svg" alt="Qitchen Logo" className="w-24 h-24 " />
+    <nav className="relative z-[999] mx-auto mt-5 flex w-full items-start justify-center px-[5%] md:mt-6 lg:mx-[5%] lg:w-auto lg:px-0">
+      <div className="flex min-h-16 w-full items-center justify-between gap-4 px-5 md:min-h-18 md:px-8 lg:w-auto">
+        <NavLink to={"/"}>
+          <img src="./Logo.svg" alt="Qitchen logo" />
         </NavLink>
-        <div>
-          <Button className="font-sans tracking-widest text-xs">
+        <div className="flex items-center gap-2">
+          <Button className="text-xs font-sans tracking-widest p-2">
             BOOK A TABLE
           </Button>
-        </div>
-        <div>
-          <Menu size={32} strokeWidth={1} />
+
+          <Menu size={32} strokeWidth={1} className="cursor-pointer" />
         </div>
       </div>
     </nav>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,54 @@
 import { headerData } from "@/assets/assets";
-import { Facebook, Instagram, Twitter } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
   return (
-    <section className="relative h-screen w-full border border-border-default rounded-2xl overflow-hidden">
-      <video
-        src="./header-vid.mp4"
-        autoPlay
-        muted
-        className="absolute -z-10 object-cover w-full h-full"
-      />
-      <div className="px-8">
-        <h1 className="text-5xl text-center">SUSHI SENSATION</h1>
+    <section id="header" className="flex flex-col gap-4">
+      <div className="relative px-[5%] rounded-2xl overflow-hidden">
+        <div className="flex h-96 items-end">
+          <div className="max-w-md flex">
+            <h1 className="text-6xl text-center text-text-default md:text-9xl mb-4">
+              SUSHI SENSATION
+            </h1>
+          </div>
+          <div className="absolute inset-0 -z-10">
+            <video
+              className="absolute inset-0 aspect-video size-full object-cover"
+              autoPlay
+              loop
+              muted
+            >
+              <source src="./header-vid.mp4" />
+            </video>
+            <div
+              className="absolute bottom-0 left-0 right-0 h-48"
+              style={{
+                background:
+                  "linear-gradient(to top, rgba(0, 0, 0, 0.8), transparent)",
+              }}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="w-full flex flex-col gap-4">
+        {headerData.map((item, index) => (
+          <div key={index} className="relative rounded-2xl overflow-hidden">
+            <div className="flex h-72">
+              <div className="self-end ml-auto">
+                <Button
+                  variant="secondary"
+                  className="text-xs font-sans tracking-widest"
+                >
+                  {item.title}
+                </Button>
+              </div>
+              <div className="absolute inset-0 -z-10">
+                <img src={item.image.src} alt={item.image.alt} className="" />
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
