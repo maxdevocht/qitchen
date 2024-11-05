dffbb2bc3440b48b1aecffb8e3175ccde52e5cae
Max de Vocht <maxdevocht@hotmail.com>
2024-11-05 13:16:52 +0100

    Created HomePage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,49 +1,5 @@
-import { motion } from "framer-motion";
 import { useState } from "react";
 import { NavLink } from "react-router-dom";
-
-const topLineVariants = {
-  open: {
-    translateY: 8,
-    transition: { delay: 0.1 },
-  },
-  rotatePhase: {
-    rotate: -45,
-    transition: { delay: 0.2 },
-  },
-  closed: {
-    translateY: 0,
-    rotate: 0,
-    transition: { duration: 0.2 },
-  },
-};
-
-const middleLineVariants = {
-  open: {
-    width: 0,
-    transition: { duration: 0.1 },
-  },
-  closed: {
-    width: "1.5rem",
-    transition: { delay: 0.3, duration: 0.2 },
-  },
-};
-
-const bottomLineVariants = {
-  open: {
-    translateY: -8,
-    transition: { delay: 0.1 },
-  },
-  rotatePhase: {
-    rotate: 45,
-    transition: { delay: 0.2 },
-  },
-  closed: {
-    translateY: 0,
-    rotate: 0,
-    transition: { duration: 0.2 },
-  },
-};
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -52,27 +8,33 @@ const Navbar = () => {
     <nav className="absolute top-12 left-12 border border-border-default rounded-lg p-2">
       <div className="flex items-center justify-between">
         <NavLink to={"/"} className="flex items-center">
-          <img src="./Logo.svg" alt="Qitchen logo" className="pl-2" />
+          <img
+            src="./Logo.svg"
+            alt="Qitchen logo"
+            className="pl-2"
+            height={120}
+            width={120}
+          />
         </NavLink>
 
         <div className="flex mr-4 ml-12">
           <ul className="flex items-center gap-4">
-            <NavLink className="font-sans font-light tracking-wider text-sm">
+            <NavLink className="font-sans font-light tracking-wider text-xs">
               MENU
             </NavLink>
-            <NavLink className="font-sans font-light tracking-wider text-sm">
+            <NavLink className="font-sans font-light tracking-wider text-xs">
               ABOUT
             </NavLink>
           </ul>
         </div>
 
         <div className="flex items-center gap-4">
-          <button className="font-sans font-light tracking-wider text-sm py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100">
+          <button className="font-sans font-light tracking-wider text-xs py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100">
             BOOK A TABLE
           </button>
 
           <button
-            className={`hamburger hamburger--minus flex items-center py-3 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100 ${
+            className={`hamburger hamburger--minus flex items-center py-2.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100 ${
               isMenuOpen ? "is-active" : ""
             }`}
             type="button"
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,26 @@
+import { Facebook, Instagram, Twitter } from "lucide-react";
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <section className="p-6 h-screen w-full">
+      <div className="w-3/4 rounded-lg border border-border-default">
+        <div className="flex items-end justify-between">
+          <h1 className="text-9xl">SUSHI SENSATION</h1>
+          <div className="flex gap-2">
+            <button className="border border-border-default bg-background-muted rounded-full p-3 hover:bg-background-muted-hover hover:border-border-default-hover transition-all duration-100">
+              <Instagram strokeWidth={1} />
+            </button>
+            <button className="border border-border-default bg-background-muted rounded-full p-3 hover:bg-background-muted-hover hover:border-border-default-hover transition-all duration-100">
+              <Facebook strokeWidth={1} />
+            </button>
+            <button className="border border-border-default bg-background-muted rounded-full p-3 hover:bg-background-muted-hover hover:border-border-default-hover transition-all duration-100">
+              <Twitter strokeWidth={1} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
