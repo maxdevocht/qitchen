78091a5151e860ed70fecad60043f8ed13ad8ebb
Max de Vocht <maxdevocht@hotmail.com>
2024-11-05 12:48:35 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,27 +52,27 @@ const Navbar = () => {
     <nav className="absolute top-12 left-12 border border-border-default rounded-lg p-2">
       <div className="flex items-center justify-between">
         <NavLink to={"/"} className="flex items-center">
-          <img src="./Logo.svg" alt="Qitchen logo" />
+          <img src="./Logo.svg" alt="Qitchen logo" className="pl-2" />
         </NavLink>
 
-        <div className="flex mr-4 ml-16">
+        <div className="flex mr-4 ml-12">
           <ul className="flex items-center gap-4">
-            <NavLink className="font-sans font-light tracking-wider">
+            <NavLink className="font-sans font-light tracking-wider text-sm">
               MENU
             </NavLink>
-            <NavLink className="font-sans font-light tracking-wider">
+            <NavLink className="font-sans font-light tracking-wider text-sm">
               ABOUT
             </NavLink>
           </ul>
         </div>
 
         <div className="flex items-center gap-4">
-          <button className="font-sans font-light tracking-wider py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100">
+          <button className="font-sans font-light tracking-wider text-sm py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100">
             BOOK A TABLE
           </button>
 
           <button
-            className={`hamburger hamburger--minus flex items-center py-3.5 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100 ${
+            className={`hamburger hamburger--minus flex items-center py-3 px-4 border border-border-default rounded-lg bg-background-muted hover:border-border-default-hover hover:bg-background-muted-hover transition-all duration-100 ${
               isMenuOpen ? "is-active" : ""
             }`}
             type="button"
